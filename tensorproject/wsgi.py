98c64e07ae41ef97be224c97304b8fc6ba2a4dd0
FroxTrost <rajatkumarchahar@gmail.com>
2020-01-10 12:16:27 +0530

    updated

--- tensorproject/wsgi.py
+++ tensorproject/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 # from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tensorproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tensorproject.tensorprojects.settings')
 
 application = get_wsgi_application()
 # application = DjangoWhiteNoise(application)
