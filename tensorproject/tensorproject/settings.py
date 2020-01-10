2d75a6365e7dc04a736ce3a7e5a5158faa7cc02d
FroxTrost <rajatkumarchahar@gmail.com>
2020-01-10 11:42:25 +0530

    static file path bug

--- tensorproject/tensorproject/settings.py
+++ tensorproject/tensorproject/settings.py
@@ -125,7 +125,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static'),
 )
 os.makedirs(STATIC_TMP, exist_ok=True)
 os.makedirs(STATIC_ROOT, exist_ok=True)
